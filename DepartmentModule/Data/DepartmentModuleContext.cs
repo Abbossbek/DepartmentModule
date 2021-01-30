7c4016b7ade4fcc9faf6dfe6a6fec8e5bde9d09a
Abbosbek Niyozqulov <abbos6882@gmail.com>
2021-01-30 19:10:05 +0500

    Добавьте файлы проекта.

---
+++ DepartmentModule/Data/DepartmentModuleContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DepartmentModule.Models;
+
+namespace DepartmentModule.Data
+{
+    public class DepartmentModuleContext : DbContext
+    {
+        public DepartmentModuleContext (DbContextOptions<DepartmentModuleContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+
+        public DbSet<DepartmentModule.Models.Book> Book { get; set; }
+
+
+        public DbSet<DepartmentModule.Models.Subject> Subject { get; set; }
+    }
+}
---
+++ DepartmentModule/Models/Subject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepartmentModule.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Book Program { get; set; }
+        public Book Themes { get; set; }
+        public List<Book> Literatures { get; set; }
+        public List<Book> AdditionalLiteratures { get; set; }
+    }
+}
