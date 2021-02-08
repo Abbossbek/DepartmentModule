17fa18e7336e639611d9a6031755469438f34a89
Abbosbek Niyozqulov <abbos6882@gmail.com>
2021-02-08 21:18:10 +0500

    update

--- DepartmentModule/Program.cs
+++ DepartmentModule/Program.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace DepartmentModule
 {
-    public class Program
+    public class Program 
     {
         public static void Main(string[] args)
         {
